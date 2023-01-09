5afc415f99a3cfd4fdb52748c50bd4328a3c677d
neenus89 <neenus@xminds.com>
2023-01-09 15:05:18 +0530

    getters and setters

--- src/main/java/org/example/Assignment3/StudentClass.java
+++ src/main/java/org/example/Assignment3/StudentClass.java
@@ -13,11 +13,23 @@ public class StudentClass {
     String name, address;
     int [] mark = new int[5];
 
-    public StudentClass(int rollno, String name, String address, int[] mark) {
-        this.rollno = rollno;
-        this.name = name;
-        this.address = address;
-        this.mark = mark;
+    Scanner sc = new Scanner(System.in);
+
+
+    public int getRollno() {
+        return rollno;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int[] getMark() {
+        return mark;
     }
 
     /**
@@ -26,27 +38,48 @@ public class StudentClass {
      * @return void
      */
     public static void main(String [] args){
+        StudentClass s = new StudentClass();
         System.out.println("Enter rollno");
-        Scanner sc = new Scanner(System.in);
-        int rollno = sc.nextInt();
+        s.setRollno();
 
         System.out.println("Enter name of student");
-        String name =sc.next();
+        s.setName();
 
         System.out.println("Enter address of student");
-        String address = sc.next();
+        s.setAddress();
 
         System.out.println("Enter marks of 5 subjects");
-        int [] mark = new int[5];
-        for(int i=0; i < 5; i++){
-            mark[i] = sc.nextInt();
-        }
+        s.setMark();
 
-        StudentClass s = new StudentClass(rollno,name,address,mark);
-        s.grade = s.gradeCalculation(mark);
-        s.showData();
+
+        s.grade = s.gradeCalculation(s.getMark());
+        System.out.println("roll no : "+s.getRollno());
+        System.out.println("name : "+s.getName());
+        System.out.println("address : "+s.getAddress());
+        for (int i : s.getMark()) {
+            System.out.println(i);
+        }
+        System.out.println("Grade : "+s.grade);
     }
 
+
+    public void setRollno() {
+        this.rollno = sc.nextInt();
+    }
+
+    public void setName() {
+        this.name = sc.next();
+    }
+
+    public void setAddress() {
+        this.address = sc.next();
+    }
+
+    public void setMark() {
+        for(int i=0; i < 5; i++) {
+            this.mark[i] = sc.nextInt();
+        }
+    }
 
     /**
      * calculation of mark and get grade
@@ -71,17 +104,5 @@ public class StudentClass {
         }
     }
 
-    /**
-     * show all data of student
-     */
-    public void showData(){
-        System.out.println("Roll number : "+this.rollno);
-        System.out.println("Name of student :"+this.name);
-        System.out.println("Address of student :"+this.address);
-        System.out.println("Marks of student : ");
-        for (int i:this.mark) {
-            System.out.println(i);
-        }
-        System.out.println("Grade of student : "+this.grade);
-    }
+
 }
