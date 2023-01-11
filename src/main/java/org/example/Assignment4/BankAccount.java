6b3597f10b9f3ebef92cd791b2b8dd36c56d4406
neenus89 <neenus@xminds.com>
2023-01-11 18:48:31 +0530

    corrected inner private class

--- src/main/java/org/example/Assignment4/BankAccount.java
+++ src/main/java/org/example/Assignment4/BankAccount.java
@@ -6,20 +6,25 @@ public class BankAccount {
     String accNumber;
     int balance , rate;
 
-   class AccountBalance {
-       public float currentBalance(){
+   private class AccountBalance {
+       public float currentBalance() {
 
-        System.out.println("Enter balance");
-        Scanner sc= new Scanner(System.in);
-        balance = sc.nextInt();
+           System.out.println("Enter balance");
+           Scanner sc = new Scanner(System.in);
+           balance = sc.nextInt();
 
-        System.out.println("Enter account number");
-        accNumber = sc.next();
-        System.out.println("Enter rate");
-        rate = sc.nextInt();
-        balance = balance + (balance*rate)/100;
-        return  balance;
-    }
+           System.out.println("Enter account number");
+           accNumber = sc.next();
+           System.out.println("Enter rate");
+           rate = sc.nextInt();
+           balance = balance + (balance * rate) / 100;
+           return balance;
+       }
+   }
+
+   void call_inner() {
+       AccountBalance bal = new AccountBalance();
+       bal.currentBalance();
    }
 
 }
@@ -27,8 +32,8 @@ public class BankAccount {
 class Main {
     public static void main(String [] args){
         BankAccount bank_account = new BankAccount();
-        BankAccount.AccountBalance account_balance = bank_account.new AccountBalance();
-        account_balance.currentBalance();
+
+        bank_account.call_inner();
         System.out.println("Account Number "+bank_account.accNumber);
         System.out.println("Rate "+bank_account.rate);
         System.out.println("Current balance "+bank_account.balance);
