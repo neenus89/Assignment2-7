0449ba59d87288d4c631c93b5b6f452d5f5ee7a8
neenus89 <neenus@xminds.com>
2023-01-11 17:10:15 +0530

    getters and setters

--- src/main/java/org/example/Assignment4/AccountBalance.java
+++
@@ -1,2 +0,0 @@
-package org.example.Assignment4;public class AccountBalance {
-}
--- src/main/java/org/example/Assignment4/BankAccount.java
+++ src/main/java/org/example/Assignment4/BankAccount.java
@@ -1,4 +1,37 @@
 package org.example.Assignment4;
 
+import java.util.*;
+
 public class BankAccount {
+    String accNumber;
+    int balance , rate;
+
+   class AccountBalance {
+       public float currentBalance(){
+
+        System.out.println("Enter balance");
+        Scanner sc= new Scanner(System.in);
+        balance = sc.nextInt();
+
+        System.out.println("Enter account number");
+        accNumber = sc.next();
+        System.out.println("Enter rate");
+        rate = sc.nextInt();
+        balance = balance + (balance*rate)/100;
+        return  balance;
+    }
+   }
+
 }
+
+class Main {
+    public static void main(String [] args){
+        BankAccount bank_account = new BankAccount();
+        BankAccount.AccountBalance account_balance = bank_account.new AccountBalance();
+        account_balance.currentBalance();
+        System.out.println("Account Number "+bank_account.accNumber);
+        System.out.println("Rate "+bank_account.rate);
+        System.out.println("Current balance "+bank_account.balance);
+    }
+}
+
