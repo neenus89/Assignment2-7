0931d04a075f961b6fd57902642aa5003daea278
neenus89 <neenus@xminds.com>
2023-03-17 16:43:49 +0530

    chnaged exception

--- src/main/java/org/example/Assignment6/NumberExceptionMain.java
+++ src/main/java/org/example/Assignment6/NumberExceptionMain.java
@@ -8,9 +8,21 @@ public class NumberExceptionMain {
         System.out.println("Enter number");
 
         int num = sc.nextInt();
+        NumberExceptionMain obj = new NumberExceptionMain();
+        try{
+            obj.check(num);
+        }
+        catch (ArithmeticException e){
+            System.out.println(e);
+        }
 
+    }
+
+    public void check(int num) {
         if (num > 999) {
             throw new ArithmeticException("Excceds 999");
         }
     }
 }
+
+
